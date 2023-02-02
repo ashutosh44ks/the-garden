4d035b1332954f1a948b2dba4496190aebb412c8
Ashutosh Kumar Singh <ashutosh44ks@gmail.com>
2023-02-02 17:22:42 +0530

    remove unnecessary code

--- client/src/components/layout/index.js
+++ client/src/components/layout/index.js
@@ -3,13 +3,13 @@ import Header from "./Header";
 
 const Layout = ({ children }) => {
   return (
-    <div className="flex flex-col">
+    <>
       <Header />
       <div className="page-template">
         <Outlet />
         {children}
       </div>
-    </div>
+    </>
   );
 };
 
