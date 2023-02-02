684dd57a386c54d0884a7fb5db659513828c4aa4
Ashutosh Kumar Singh <ashutosh44ks@gmail.com>
2023-02-02 17:34:52 +0530

    update design

--- client/src/pages/Subject.js
+++ client/src/pages/Subject.js
@@ -49,61 +49,66 @@ const Subject = () => {
   if (Object.keys(subject) === 0)
     return <div className="p-8">Subject not found</div>;
   return (
-    <div className="p-8 bg-white">
-      <div className="flex items-center mb-2 justify-between">
-        <div className="flex items-end text-dark">
-          <h1 className="text-2xl mr-2">{subject.name}</h1>
-          <span>{subject.code}</span>
-        </div>
-        <button className="btn-primary">Update Details</button>
-      </div>
-      <p className="text-dark text-sm mb-4 w-3/4">{subject.description}</p>
-      <div className="mb-4">
-        <span className="simple-tab tab-theme-blue">
-          {addSuffix(subject.year)} year
-        </span>
-        <span
-          className={`simple-tab ${
-            subject.credits > 4 ? "tab-theme-red" : ""
-          } ${subject.credits === 4 ? "tab-theme-orange" : ""}
+    <div className="p-8">
+      <div className="card">
+        <div className="card-body">
+          <div className="flex items-center mb-2 justify-between">
+            <div className="flex items-end text-dark">
+              <h1 className="text-2xl mr-2">{subject.name}</h1>
+              <span>{subject.code}</span>
+            </div>
+            <button className="btn-primary">Update Details</button>
+          </div>
+          <p className="text-dark text-sm mb-4 w-3/4">{subject.description}</p>
+          <div className="mb-4">
+            <span className="simple-tab tab-theme-blue">
+              {addSuffix(subject.year)} year
+            </span>
+            <span
+              className={`simple-tab ${
+                subject.credits > 4 ? "tab-theme-red" : ""
+              } ${subject.credits === 4 ? "tab-theme-orange" : ""}
           ${subject.credits === 3 ? "tab-theme-yellow" : ""}
           ${subject.credits < 3 ? "tab-theme-green" : ""}
           `}
-        >
-          {subject.credits} credits
-        </span>
-        <span
-          className={`mr-2 text-sm simple-tab ${
-            subject.gate ? "tab-theme-red" : "tab-theme-green"
-          }`}
-        >
-          {subject.gate ? "Gate Subject" : "Non-Gate Subject"}
-        </span>
-        <span
-          className={`mr-2 text-sm simple-tab ${
-            subject.practicals ? "tab-theme-yellow" : "tab-theme-green"
-          }`}
-        >
-          {subject.practicals ? "Practical subject" : "Theoretical subject"}
-        </span>
-        <span
-          className={`simple-tab ${
-            subject.difficulty > 4 ? "tab-theme-red" : ""
-          } ${subject.difficulty === 4 ? "tab-theme-orange" : ""}
+            >
+              {subject.credits} credits
+            </span>
+            <span
+              className={`mr-2 text-sm simple-tab ${
+                subject.gate ? "tab-theme-red" : "tab-theme-green"
+              }`}
+            >
+              {subject.gate ? "Gate Subject" : "Non-Gate Subject"}
+            </span>
+            <span
+              className={`mr-2 text-sm simple-tab ${
+                subject.practicals ? "tab-theme-yellow" : "tab-theme-green"
+              }`}
+            >
+              {subject.practicals ? "Practical subject" : "Theoretical subject"}
+            </span>
+            <span
+              className={`simple-tab ${
+                subject.difficulty > 4 ? "tab-theme-red" : ""
+              } ${subject.difficulty === 4 ? "tab-theme-orange" : ""}
           ${subject.difficulty === 3 ? "tab-theme-yellow" : ""}
           ${subject.difficulty < 3 ? "tab-theme-green" : ""}
           `}
-        >
-          {numToLevel(subject.difficulty)} difficulty
-        </span>
-      </div>
-      <div className="mb-4">
-        <h3>Professor History</h3>
-        <div className="text-dark text-sm mb-2">
-          Ratings ranges from 1 (bad) to 5 (good)
+            >
+              {numToLevel(subject.difficulty)} difficulty
+            </span>
+          </div>
         </div>
-        <div
-          className="
+      </div>
+      <div className="py-4">
+        <div className="mb-4">
+          <h3>Professor History</h3>
+          <div className="text-dark text-sm mb-2">
+            Ratings ranges from 1 (bad) to 5 (good)
+          </div>
+          <div
+            className="
         grid
         grid-cols-1
         md:grid-cols-2
@@ -111,70 +116,71 @@ const Subject = () => {
         xl:grid-cols-4
         gap-4
         "
-        >
-          {subject.professors.map((professor) => {
-            return (
-              <div className="card" key={professor.code}>
-                <div className="card-body">
-                  <h3 className="card-title">
-                    {`${professor.name} (${professor.code})`}
-                  </h3>
-                  <div className="text-dark text-sm mb-2">
-                    {professor.designation +
-                      ", " +
-                      (professor.active
-                        ? "Current lecturer"
-                        : "Former lecturer")}
-                  </div>
-                  {Object.entries(professor.ratings).map(([key, value]) => {
-                    return (
-                      <div className="flex items-center" key={key}>
-                        <span className="w-[10rem]">{toLabel(key)}:</span>
-                        <span className="mr-1">{value}</span>
-                        <AiFillStar className="text-blue" />
-                      </div>
-                    );
-                  })}
-                  <div className="flex items-center">
-                    <span className="w-[10rem]">Average Rating:</span>
-                    <span className="mr-1">
-                      {calcAvgRating(professor.ratings)}
-                    </span>
-                    <AiFillStar className="text-blue" />
+          >
+            {subject.professors.map((professor) => {
+              return (
+                <div className="card" key={professor.code}>
+                  <div className="card-body">
+                    <h3 className="card-title">
+                      {`${professor.name} (${professor.code})`}
+                    </h3>
+                    <div className="text-dark text-sm mb-2">
+                      {professor.designation +
+                        ", " +
+                        (professor.active
+                          ? "Current lecturer"
+                          : "Former lecturer")}
+                    </div>
+                    {Object.entries(professor.ratings).map(([key, value]) => {
+                      return (
+                        <div className="flex items-center" key={key}>
+                          <span className="w-[10rem]">{toLabel(key)}:</span>
+                          <span className="mr-1">{value}</span>
+                          <AiFillStar className="text-blue" />
+                        </div>
+                      );
+                    })}
+                    <div className="flex items-center">
+                      <span className="w-[10rem]">Average Rating:</span>
+                      <span className="mr-1">
+                        {calcAvgRating(professor.ratings)}
+                      </span>
+                      <AiFillStar className="text-blue" />
+                    </div>
                   </div>
                 </div>
-              </div>
-            );
-          })}
+              );
+            })}
+          </div>
         </div>
-      </div>
-      <div>
-        <h3 className="mb-4">Resources</h3>
-        <div className="flex gap-4">
-          <button
-            className="btn-secondary"
-            onClick={() => {
-              navigate(`/subject/${subjectId}/syllabus`);
-            }}
-          >
-            Syllabus
-          </button>
-          <button
-            className="btn-secondary"
-            onClick={() => {
-              navigate(`/subject/${subjectId}/notes`);
-            }}
-          >
-            Notes
-          </button>
-          <button
-            className="btn-secondary"
-            onClick={() => {
-              navigate(`/subject/${subjectId}/question_papers`);
-            }}
-          >
-            Previous Year Papers
-          </button>
+        <div>
+          <h3 className="mb-4">Resources</h3>
+          <div className="flex gap-4">
+            <button
+              className="btn-secondary"
+              onClick={() => {
+                navigate(`/subject/${subjectId}/syllabus`);
+              }}
+            >
+              Syllabus
+            </button>
+            <button
+              className="btn-secondary"
+              onClick={() => {
+                navigate(`/subject/${subjectId}/notes`);
+              }}
+            >
+              Notes
+            </button>
+            <button
+              className="btn-secondary"
+              onClick={() => {
+                navigate(`/subject/${subjectId}/question_papers`);
+              }}
+            >
+              Previous Year Papers
+            </button>
+          </div>
         </div>
       </div>
     </div>
