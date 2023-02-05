eabb67d92df3c06aea5acb5fa447252f03f726a0
Ashutosh Kumar Singh <ashutosh44ks@gmail.com>
2023-02-05 18:25:56 +0530

    fix bug

--- client/src/components/utils/ProtectedRoute.js
+++ client/src/components/utils/ProtectedRoute.js
@@ -14,7 +14,7 @@ const ProtectedRoute = ({ children }) => {
           JSON.parse(localStorage.getItem("logged")).username
         }&password=${JSON.parse(localStorage.getItem("logged")).password}`
       );
-      console.log(data);
+      localStorage.setItem("logged", JSON.stringify(data.user));
       setIsAuthenticated(true);
     } catch (e) {
       console.log(e);
