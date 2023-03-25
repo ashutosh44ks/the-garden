85bc39486c5362bbb8eb8424dc9356bc36f0412e
Ashutosh Kumar Singh <ashutosh44ks@gmail.com>
2023-03-25 14:31:16 +0530

    Update TextArea.js

--- client/src/components/common/MUI-themed/TextArea.js
+++ client/src/components/common/MUI-themed/TextArea.js
@@ -1,6 +1,16 @@
+import { useRef, useEffect } from "react";
 import "./MUI-themed.css";
 
 const TextArea = ({ label, rows, val, setVal, required, className }) => {
+  const materialInput = useRef(null);
+  const materialLabel = useRef(null);
+
+  useEffect(() => {
+    materialLabel.current.addEventListener("click", () => {
+      materialInput.current.focus();
+    });
+  }, []);
+
   return (
     <div class={"input-group " + className}>
       <textarea
@@ -9,8 +19,9 @@ const TextArea = ({ label, rows, val, setVal, required, className }) => {
         onChange={(e) => setVal(e.target.value)}
         placeholder="."
         required={required}
+        ref={materialInput}
       />
-      <label>{label}</label>
+      <label ref={materialLabel}>{label}</label>
     </div>
   );
 };
