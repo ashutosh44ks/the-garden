9e60a7d6c0ce7e37f13f50782432e3b1513f9227
Ashutosh Kumar Singh <ashutosh44ks@gmail.com>
2023-03-31 21:52:04 +0530

    Update index.js

--- client/src/pages/Subject/index.js
+++ client/src/pages/Subject/index.js
@@ -3,7 +3,6 @@ import { useNavigate, useParams } from "react-router-dom";
 import api from "../../components/utils/api";
 import toLabel from "../../components/utils/toLabel";
 import "./Subject.css";
-import StarRating from "../../components/common/StarRating";
 
 const Subject = () => {
   const navigate = useNavigate();
