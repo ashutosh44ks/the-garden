5e9eca0200c95b2f648d6c0aa3d4f2caf8d7a4f7
Ashutosh Kumar Singh <ashutosh44ks@gmail.com>
2023-09-16 16:47:03 +0530

    Update return navigation link

--- client/src/pages/SubjectFileHandling/SubjectUpload.js
+++ client/src/pages/SubjectFileHandling/SubjectUpload.js
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import { useParams, useNavigate } from "react-router-dom";
 import jwt_decode from "jwt-decode";
 import api from "../../components/utils/api";
+import toLabel from "../../components/utils/toLabel";
 import {
   uploadFileToStorage,
   createFilePath,
@@ -107,10 +108,10 @@ const SubjectUpload = () => {
           <u
             className="text-blue cursor-pointer"
             onClick={() => {
-              navigate(`/subject/${subjectId}`);
+              navigate(`/subject/${subjectId}/${category}/view`);
             }}
           >
-            {subjectId} page
+            {toLabel(category)} page
           </u>
         </div>
         <p className="text-dark-2">
